7436f0a930f38db46dd7328c5f470431969c985a
Farhan Akhtar <farhanakhter74786@gmail.com>
2025-10-18 22:03:55 +0530

    Add Files

--- models/listings.js
+++ models/listings.js
@@ -25,17 +25,17 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User"
     },
-    geometry: {
-        type: {
-            type: String, // Don't do `{ location: { type: String } }`
-            enum: ['Point'], // 'location.type' must be 'Point'
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
-    },
+    // geometry: {
+    //     type: {
+    //         type: String, // Don't do `{ location: { type: String } }`
+    //         enum: ['Point'], // 'location.type' must be 'Point'
+    //         required: true
+    //     },
+    //     coordinates: {
+    //         type: [Number],
+    //         required: true
+    //     }
+    // },
     category: {
         type: String,
         enum: ["Trending", "Rooms", "Iconic Cities", "Mountains", 
